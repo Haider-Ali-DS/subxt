6c7caddb1037a37fa9807426782b883dc08e35f9
Haider-Ali-DS <haiderali.tech_786@outlook.com>
2022-10-28 03:22:29 +0500

    initial commit

---
+++ codegen/src/api/mod.rs
@@ -0,0 +1,376 @@
+// Copyright 2019-2022 Parity Technologies (UK) Ltd.
+// This file is dual-licensed as Apache-2.0 or GPL-3.0.
+// see LICENSE for license details.
+
+//! Generate code for submitting extrinsics and query storage of a Substrate runtime.
+
+mod calls;
+mod constants;
+mod events;
+mod storage;
+
+use subxt_metadata::get_metadata_per_pallet_hash;
+
+use super::DerivesRegistry;
+use crate::{
+    ir,
+    types::{
+        CompositeDef,
+        CompositeDefFields,
+        TypeGenerator,
+    },
+};
+use codec::Decode;
+use frame_metadata::{
+    v14::RuntimeMetadataV14,
+    RuntimeMetadata,
+    RuntimeMetadataPrefixed,
+};
+use heck::ToSnakeCase as _;
+use proc_macro2::TokenStream as TokenStream2;
+use proc_macro_error::abort_call_site;
+use quote::{
+    format_ident,
+    quote,
+};
+use std::{
+    collections::HashMap,
+    fs,
+    io::Read,
+    path,
+    string::ToString,
+};
+use syn::parse_quote;
+
+/// Generates the API for interacting with a Substrate runtime.
+///
+/// # Arguments
+///
+/// * `item_mod` - The module declaration for which the API is implemented.
+/// * `path` - The path to the scale encoded metadata of the runtime node.
+/// * `derives` - Provide custom derives for the generated types.
+///
+/// **Note:** This is a wrapper over [RuntimeGenerator] for static metadata use-cases.
+pub fn generate_runtime_api<P>(
+    item_mod: syn::ItemMod,
+    path: P,
+    derives: DerivesRegistry,
+) -> TokenStream2
+where
+    P: AsRef<path::Path>,
+{
+    let mut file = fs::File::open(&path).unwrap_or_else(|e| {
+        abort_call_site!("Failed to open {}: {}", path.as_ref().to_string_lossy(), e)
+    });
+
+    let mut bytes = Vec::new();
+    file.read_to_end(&mut bytes)
+        .unwrap_or_else(|e| abort_call_site!("Failed to read metadata file: {}", e));
+
+    let metadata = frame_metadata::RuntimeMetadataPrefixed::decode(&mut &bytes[..])
+        .unwrap_or_else(|e| abort_call_site!("Failed to decode metadata: {}", e));
+
+    let generator = RuntimeGenerator::new(metadata);
+    generator.generate_runtime(item_mod, derives)
+}
+
+/// Create the API for interacting with a Substrate runtime.
+pub struct RuntimeGenerator {
+    metadata: RuntimeMetadataV14,
+}
+
+impl RuntimeGenerator {
+    /// Create a new runtime generator from the provided metadata.
+    ///
+    /// **Note:** If you have a path to the metadata, prefer to use [generate_runtime_api]
+    /// for generating the runtime API.
+    pub fn new(metadata: RuntimeMetadataPrefixed) -> Self {
+        match metadata.1 {
+            RuntimeMetadata::V14(v14) => Self { metadata: v14 },
+            _ => panic!("Unsupported metadata version {:?}", metadata.1),
+        }
+    }
+
+    /// Generate the API for interacting with a Substrate runtime.
+    ///
+    /// # Arguments
+    ///
+    /// * `item_mod` - The module declaration for which the API is implemented.
+    /// * `derives` - Provide custom derives for the generated types.
+    pub fn generate_runtime(
+        &self,
+        item_mod: syn::ItemMod,
+        derives: DerivesRegistry,
+    ) -> TokenStream2 {
+        let item_mod_ir = ir::ItemMod::from(item_mod);
+        let default_derives = derives.default_derives();
+
+        // Some hardcoded default type substitutes, can be overridden by user
+        let mut type_substitutes = [
+            (
+                "bitvec::order::Lsb0",
+                parse_quote!(::subxt::ext::bitvec::order::Lsb0),
+            ),
+            (
+                "bitvec::order::Msb0",
+                parse_quote!(::subxt::ext::bitvec::order::Msb0),
+            ),
+            (
+                "sp_core::crypto::AccountId32",
+                parse_quote!(::subxt::ext::sp_core::crypto::AccountId32),
+            ),
+            (
+                "primitive_types::H160",
+                parse_quote!(::subxt::ext::sp_core::H160),
+            ),
+            (
+                "primitive_types::H256",
+                parse_quote!(::subxt::ext::sp_core::H256),
+            ),
+            (
+                "primitive_types::H512",
+                parse_quote!(::subxt::ext::sp_core::H512),
+            ),
+            (
+                "sp_runtime::multiaddress::MultiAddress",
+                parse_quote!(::subxt::ext::sp_runtime::MultiAddress),
+            ),
+            (
+                "frame_support::traits::misc::WrapperKeepOpaque",
+                parse_quote!(::subxt::utils::WrapperKeepOpaque),
+            ),
+            // BTreeMap and BTreeSet impose an `Ord` constraint on their key types. This
+            // can cause an issue with generated code that doesn't impl `Ord` by default.
+            // Decoding them to Vec by default (KeyedVec is just an alias for Vec with
+            // suitable type params) avoids these issues.
+            ("BTreeMap", parse_quote!(::subxt::utils::KeyedVec)),
+            ("BTreeSet", parse_quote!(::std::vec::Vec)),
+        ]
+        .iter()
+        .map(|(path, substitute): &(&str, syn::TypePath)| {
+            (path.to_string(), substitute.clone())
+        })
+        .collect::<HashMap<_, _>>();
+
+        for (path, substitute) in item_mod_ir.type_substitutes().iter() {
+            type_substitutes.insert(path.to_string(), substitute.clone());
+        }
+
+        let type_gen = TypeGenerator::new(
+            &self.metadata.types,
+            "runtime_types",
+            type_substitutes,
+            derives.clone(),
+        );
+        let types_mod = type_gen.generate_types_mod();
+        let types_mod_ident = types_mod.ident();
+        let pallets_with_mod_names = self
+            .metadata
+            .pallets
+            .iter()
+            .map(|pallet| {
+                (
+                    pallet,
+                    format_ident!("{}", pallet.name.to_string().to_snake_case()),
+                )
+            })
+            .collect::<Vec<_>>();
+
+        // Pallet names and their length are used to create PALLETS array.
+        // The array is used to identify the pallets composing the metadata for
+        // validation of just those pallets.
+        let pallet_names: Vec<_> = self
+            .metadata
+            .pallets
+            .iter()
+            .map(|pallet| &pallet.name)
+            .collect();
+        let pallet_names_len = pallet_names.len();
+
+        let metadata_hash = get_metadata_per_pallet_hash(&self.metadata, &pallet_names);
+
+        let modules = pallets_with_mod_names.iter().map(|(pallet, mod_name)| {
+            let calls =
+                calls::generate_calls(&self.metadata, &type_gen, pallet, types_mod_ident);
+
+            let event = events::generate_events(&type_gen, pallet, types_mod_ident);
+
+            let storage_mod = storage::generate_storage(
+                &self.metadata,
+                &type_gen,
+                pallet,
+                types_mod_ident,
+            );
+
+            let constants_mod = constants::generate_constants(
+                &self.metadata,
+                &type_gen,
+                pallet,
+                types_mod_ident,
+            );
+
+            quote! {
+                pub mod #mod_name {
+                    use super::root_mod;
+                    use super::#types_mod_ident;
+                    #calls
+                    #event
+                    #storage_mod
+                    #constants_mod
+                }
+            }
+        });
+
+        let outer_event_variants = self.metadata.pallets.iter().filter_map(|p| {
+            let variant_name = format_ident!("{}", p.name);
+            let mod_name = format_ident!("{}", p.name.to_string().to_snake_case());
+            let index = proc_macro2::Literal::u8_unsuffixed(p.index);
+
+            p.event.as_ref().map(|_| {
+                quote! {
+                    #[codec(index = #index)]
+                    #variant_name(#mod_name::Event),
+                }
+            })
+        });
+
+        let outer_event = quote! {
+            #default_derives
+            pub enum Event {
+                #( #outer_event_variants )*
+            }
+        };
+
+        let mod_ident = item_mod_ir.ident;
+        let pallets_with_constants: Vec<_> = pallets_with_mod_names
+            .iter()
+            .filter_map(|(pallet, pallet_mod_name)| {
+                (!pallet.constants.is_empty()).then_some(pallet_mod_name)
+            })
+            .collect();
+
+        let pallets_with_storage: Vec<_> = pallets_with_mod_names
+            .iter()
+            .filter_map(|(pallet, pallet_mod_name)| {
+                pallet.storage.as_ref().map(|_| pallet_mod_name)
+            })
+            .collect();
+
+        let pallets_with_calls: Vec<_> = pallets_with_mod_names
+            .iter()
+            .filter_map(|(pallet, pallet_mod_name)| {
+                pallet.calls.as_ref().map(|_| pallet_mod_name)
+            })
+            .collect();
+
+        quote! {
+            #[allow(dead_code, unused_imports, non_camel_case_types)]
+            pub mod #mod_ident {
+                // Make it easy to access the root via `root_mod` at different levels:
+                use super::#mod_ident as root_mod;
+                // Identify the pallets composing the static metadata by name.
+                pub static PALLETS: [&str; #pallet_names_len] = [ #(#pallet_names,)* ];
+
+                #outer_event
+                #( #modules )*
+                #types_mod
+
+                /// The default error type returned when there is a runtime issue,
+                /// exposed here for ease of use.
+                pub type DispatchError = #types_mod_ident::sp_runtime::DispatchError;
+
+                pub fn constants() -> ConstantsApi {
+                    ConstantsApi
+                }
+
+                pub fn storage() -> StorageApi {
+                    StorageApi
+                }
+
+                pub fn tx() -> TransactionApi {
+                    TransactionApi
+                }
+
+                pub struct ConstantsApi;
+                impl ConstantsApi {
+                    #(
+                        pub fn #pallets_with_constants(&self) -> #pallets_with_constants::constants::ConstantsApi {
+                            #pallets_with_constants::constants::ConstantsApi
+                        }
+                    )*
+                }
+
+                pub struct StorageApi;
+                impl StorageApi {
+                    #(
+                        pub fn #pallets_with_storage(&self) -> #pallets_with_storage::storage::StorageApi {
+                            #pallets_with_storage::storage::StorageApi
+                        }
+                    )*
+                }
+
+                pub struct TransactionApi;
+                impl TransactionApi {
+                    #(
+                        pub fn #pallets_with_calls(&self) -> #pallets_with_calls::calls::TransactionApi {
+                            #pallets_with_calls::calls::TransactionApi
+                        }
+                    )*
+                }
+
+                /// check whether the Client you are using is aligned with the statically generated codegen.
+                pub fn validate_codegen<T: ::subxt::Config, C: ::subxt::client::OfflineClientT<T>>(client: &C) -> Result<(), ::subxt::error::MetadataError> {
+                    let runtime_metadata_hash = client.metadata().metadata_hash(&PALLETS);
+                    if runtime_metadata_hash != [ #(#metadata_hash,)* ] {
+                        Err(::subxt::error::MetadataError::IncompatibleMetadata)
+                    } else {
+                        Ok(())
+                    }
+                }
+            }
+        }
+    }
+}
+
+/// Return a vector of tuples of variant names and corresponding struct definitions.
+pub fn generate_structs_from_variants<'a, F>(
+    type_gen: &'a TypeGenerator,
+    type_id: u32,
+    variant_to_struct_name: F,
+    error_message_type_name: &str,
+) -> Vec<(String, CompositeDef)>
+where
+    F: Fn(&str) -> std::borrow::Cow<str>,
+{
+    let ty = type_gen.resolve_type(type_id);
+    if let scale_info::TypeDef::Variant(variant) = ty.type_def() {
+        variant
+            .variants()
+            .iter()
+            .map(|var| {
+                let struct_name = variant_to_struct_name(var.name());
+                let fields = CompositeDefFields::from_scale_info_fields(
+                    struct_name.as_ref(),
+                    var.fields(),
+                    &[],
+                    type_gen,
+                );
+                let struct_def = CompositeDef::struct_def(
+                    &ty,
+                    struct_name.as_ref(),
+                    Default::default(),
+                    fields,
+                    Some(parse_quote!(pub)),
+                    type_gen,
+                    var.docs(),
+                );
+                (var.name().to_string(), struct_def)
+            })
+            .collect()
+    } else {
+        abort_call_site!(
+            "{} type should be an variant/enum type",
+            error_message_type_name
+        )
+    }
+}
---
+++ codegen/src/types/mod.rs
@@ -0,0 +1,360 @@
+// Copyright 2019-2022 Parity Technologies (UK) Ltd.
+// This file is dual-licensed as Apache-2.0 or GPL-3.0.
+// see LICENSE for license details.
+
+mod composite_def;
+mod derives;
+#[cfg(test)]
+mod tests;
+mod type_def;
+mod type_def_params;
+mod type_path;
+
+use proc_macro2::{
+    Ident,
+    Span,
+    TokenStream,
+};
+use proc_macro_error::abort_call_site;
+use quote::{
+    quote,
+    ToTokens,
+};
+use scale_info::{
+    form::PortableForm,
+    PortableRegistry,
+    Type,
+    TypeDef,
+};
+use std::collections::{
+    BTreeMap,
+    HashMap,
+};
+
+pub use self::{
+    composite_def::{
+        CompositeDef,
+        CompositeDefFieldType,
+        CompositeDefFields,
+    },
+    derives::{
+        Derives,
+        DerivesRegistry,
+    },
+    type_def::TypeDefGen,
+    type_def_params::TypeDefParameters,
+    type_path::{
+        TypeParameter,
+        TypePath,
+        TypePathType,
+    },
+};
+
+pub type Field = scale_info::Field<PortableForm>;
+
+/// Generate a Rust module containing all types defined in the supplied [`PortableRegistry`].
+#[derive(Debug)]
+pub struct TypeGenerator<'a> {
+    /// The name of the module which will contain the generated types.
+    types_mod_ident: Ident,
+    /// Registry of type definitions to be transformed into Rust type definitions.
+    type_registry: &'a PortableRegistry,
+    /// User defined overrides for generated types.
+    type_substitutes: HashMap<String, syn::TypePath>,
+    /// Set of derives with which to annotate generated types.
+    derives: DerivesRegistry,
+}
+
+impl<'a> TypeGenerator<'a> {
+    /// Construct a new [`TypeGenerator`].
+    pub fn new(
+        type_registry: &'a PortableRegistry,
+        root_mod: &'static str,
+        type_substitutes: HashMap<String, syn::TypePath>,
+        derives: DerivesRegistry,
+    ) -> Self {
+        let root_mod_ident = Ident::new(root_mod, Span::call_site());
+        Self {
+            types_mod_ident: root_mod_ident,
+            type_registry,
+            type_substitutes,
+            derives,
+        }
+    }
+
+    /// Generate a module containing all types defined in the supplied type registry.
+    pub fn generate_types_mod(&self) -> Module {
+        let mut root_mod =
+            Module::new(self.types_mod_ident.clone(), self.types_mod_ident.clone());
+
+        for (id, ty) in self.type_registry.types().iter().enumerate() {
+            if ty.ty().path().namespace().is_empty() {
+                // prelude types e.g. Option/Result have no namespace, so we don't generate them
+                continue
+            }
+            self.insert_type(
+                ty.ty().clone(),
+                id as u32,
+                ty.ty().path().namespace().to_vec(),
+                &self.types_mod_ident,
+                &mut root_mod,
+            )
+        }
+
+        root_mod
+    }
+
+    fn insert_type(
+        &'a self,
+        ty: Type<PortableForm>,
+        id: u32,
+        path: Vec<String>,
+        root_mod_ident: &Ident,
+        module: &mut Module,
+    ) {
+        let joined_path = path.join("::");
+        if self.type_substitutes.contains_key(&joined_path) {
+            return
+        }
+
+        let segment = path.first().expect("path has at least one segment");
+        let mod_ident = Ident::new(segment, Span::call_site());
+
+        let child_mod = module
+            .children
+            .entry(mod_ident.clone())
+            .or_insert_with(|| Module::new(mod_ident, root_mod_ident.clone()));
+
+        if path.len() == 1 {
+            child_mod
+                .types
+                .insert(ty.path().clone(), TypeDefGen::from_type(ty, self));
+        } else {
+            self.insert_type(ty, id, path[1..].to_vec(), root_mod_ident, child_mod)
+        }
+    }
+
+    /// # Panics
+    ///
+    /// If no type with the given id found in the type registry.
+    pub fn resolve_type(&self, id: u32) -> Type<PortableForm> {
+        self.type_registry
+            .resolve(id)
+            .unwrap_or_else(|| panic!("No type with id {} found", id))
+            .clone()
+    }
+
+    /// Get the type path for a field of a struct or an enum variant, providing any generic
+    /// type parameters from the containing type. This is for identifying where a generic type
+    /// parameter is used in a field type e.g.
+    ///
+    /// ```rust
+    /// struct S<T> {
+    ///     a: T, // `T` is the "parent" type param from the containing type.
+    ///     b: Vec<Option<T>>, // nested use of generic type param `T`.
+    /// }
+    /// ```
+    ///
+    /// This allows generating the correct generic field type paths.
+    ///
+    /// # Panics
+    ///
+    /// If no type with the given id found in the type registry.
+    pub fn resolve_field_type_path(
+        &self,
+        id: u32,
+        parent_type_params: &[TypeParameter],
+    ) -> TypePath {
+        self.resolve_type_path_recurse(id, true, parent_type_params)
+    }
+
+    /// Get the type path for the given type identifier.
+    ///
+    /// # Panics
+    ///
+    /// If no type with the given id found in the type registry.
+    pub fn resolve_type_path(&self, id: u32) -> TypePath {
+        self.resolve_type_path_recurse(id, false, &[])
+    }
+
+    /// Visit each node in a possibly nested type definition to produce a type path.
+    ///
+    /// e.g `Result<GenericStruct<NestedGenericStruct<T>>, String>`
+    fn resolve_type_path_recurse(
+        &self,
+        id: u32,
+        is_field: bool,
+        parent_type_params: &[TypeParameter],
+    ) -> TypePath {
+        if let Some(parent_type_param) = parent_type_params
+            .iter()
+            .find(|tp| tp.concrete_type_id == id)
+        {
+            return TypePath::Parameter(parent_type_param.clone())
+        }
+
+        let mut ty = self.resolve_type(id);
+
+        if ty.path().ident() == Some("Cow".to_string()) {
+            ty = self.resolve_type(
+                ty.type_params()[0]
+                    .ty()
+                    .expect("type parameters to Cow are not expected to be skipped; qed")
+                    .id(),
+            )
+        }
+
+        let params = ty
+            .type_params()
+            .iter()
+            .filter_map(|f| {
+                f.ty().map(|f| {
+                    self.resolve_type_path_recurse(f.id(), false, parent_type_params)
+                })
+            })
+            .collect();
+
+        let ty = match ty.type_def() {
+            TypeDef::Composite(_) | TypeDef::Variant(_) => {
+                let joined_path = ty.path().segments().join("::");
+                if let Some(substitute_type_path) =
+                    self.type_substitutes.get(&joined_path)
+                {
+                    TypePathType::Path {
+                        path: substitute_type_path.clone(),
+                        params,
+                    }
+                } else {
+                    TypePathType::from_type_def_path(
+                        ty.path(),
+                        self.types_mod_ident.clone(),
+                        params,
+                    )
+                }
+            }
+            TypeDef::Primitive(primitive) => {
+                TypePathType::Primitive {
+                    def: primitive.clone(),
+                }
+            }
+            TypeDef::Array(arr) => {
+                TypePathType::Array {
+                    len: arr.len() as usize,
+                    of: Box::new(self.resolve_type_path_recurse(
+                        arr.type_param().id(),
+                        false,
+                        parent_type_params,
+                    )),
+                }
+            }
+            TypeDef::Sequence(seq) => {
+                TypePathType::Vec {
+                    of: Box::new(self.resolve_type_path_recurse(
+                        seq.type_param().id(),
+                        false,
+                        parent_type_params,
+                    )),
+                }
+            }
+            TypeDef::Tuple(tuple) => {
+                TypePathType::Tuple {
+                    elements: tuple
+                        .fields()
+                        .iter()
+                        .map(|f| {
+                            self.resolve_type_path_recurse(
+                                f.id(),
+                                false,
+                                parent_type_params,
+                            )
+                        })
+                        .collect(),
+                }
+            }
+            TypeDef::Compact(compact) => {
+                TypePathType::Compact {
+                    inner: Box::new(self.resolve_type_path_recurse(
+                        compact.type_param().id(),
+                        false,
+                        parent_type_params,
+                    )),
+                    is_field,
+                }
+            }
+            TypeDef::BitSequence(bitseq) => {
+                TypePathType::BitVec {
+                    bit_order_type: Box::new(self.resolve_type_path_recurse(
+                        bitseq.bit_order_type().id(),
+                        false,
+                        parent_type_params,
+                    )),
+                    bit_store_type: Box::new(self.resolve_type_path_recurse(
+                        bitseq.bit_store_type().id(),
+                        false,
+                        parent_type_params,
+                    )),
+                }
+            }
+        };
+
+        TypePath::Type(ty)
+    }
+
+    /// Returns the derives to be applied to all generated types.
+    pub fn default_derives(&self) -> &Derives {
+        self.derives.default_derives()
+    }
+
+    /// Returns the derives to be applied to a generated type.
+    pub fn type_derives(&self, ty: &Type<PortableForm>) -> Derives {
+        let joined_path = ty.path().segments().join("::");
+        let ty_path: syn::TypePath = syn::parse_str(&joined_path).unwrap_or_else(|e| {
+            abort_call_site!("'{}' is an invalid type path: {:?}", joined_path, e,)
+        });
+        self.derives.resolve(&ty_path)
+    }
+}
+
+/// Represents a Rust `mod`, containing generated types and child `mod`s.
+#[derive(Debug)]
+pub struct Module {
+    name: Ident,
+    root_mod: Ident,
+    children: BTreeMap<Ident, Module>,
+    types: BTreeMap<scale_info::Path<PortableForm>, TypeDefGen>,
+}
+
+impl ToTokens for Module {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let name = &self.name;
+        let root_mod = &self.root_mod;
+        let modules = self.children.values();
+        let types = self.types.values().clone();
+
+        tokens.extend(quote! {
+            pub mod #name {
+                use super::#root_mod;
+
+                #( #modules )*
+                #( #types )*
+            }
+        })
+    }
+}
+
+impl Module {
+    /// Create a new [`Module`], with a reference to the root `mod` for resolving type paths.
+    pub(crate) fn new(name: Ident, root_mod: Ident) -> Self {
+        Self {
+            name,
+            root_mod,
+            children: BTreeMap::new(),
+            types: BTreeMap::new(),
+        }
+    }
+
+    /// Returns the module ident.
+    pub fn ident(&self) -> &Ident {
+        &self.name
+    }
+}
